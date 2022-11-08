45b1f919e89ea65709a97dd32cc63646a1fa96f4
risaadodx <wrisaad@gmail.com>
2022-11-08 18:08:20 +0600

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../../../assets/images/guide.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={img} className="w-2/3 rounded-lg " />
+          <div>
+            <h1 className="text-5xl font-bold">Find Your Tour Guide!</h1>
+            <p className="py-6">
+              Explore the greatest local guides our services. You won’t be
+              disappointed
+            </p>
+            <Link>
+              <button className="btn btn-success hover:btn-accent">
+                Join Now
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
