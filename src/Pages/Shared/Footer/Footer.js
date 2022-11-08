d3712041678b5dda5f033e5ddd5e8957e7a24eff
risaadodx <wrisaad@gmail.com>
2022-11-08 17:44:26 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,32 @@
 import React from "react";
+import logo from "../../../assets/logo.png";
+import { FaTwitter, FaYoutube, FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <div>
+      <footer className="footer p-20 bg-neutral text-neutral-content flex justify-between align-middle">
+        <div>
+          <img style={{ width: "120px" }} src={logo} alt="" />
+        </div>
+        <p>Copyrights: Tourella | 2022.</p>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <Link>
+              <FaTwitter className="text-2xl"></FaTwitter>
+            </Link>
+            <Link>
+              <FaFacebook className="text-2xl"></FaFacebook>
+            </Link>
+            <Link>
+              <FaYoutube className="text-2xl"></FaYoutube>
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
