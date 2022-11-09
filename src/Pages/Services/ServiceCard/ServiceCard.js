fe7d006057d1f33f0cc74898a0c34458bcdc92e5
risaadodx <wrisaad@gmail.com>
2022-11-09 22:30:52 +0600

    photo provider added

--- src/Pages/Services/ServiceCard/ServiceCard.js
+++ src/Pages/Services/ServiceCard/ServiceCard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "react-photo-view/dist/react-photo-view.css";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const ServiceCard = ({ service }) => {
   const { _id, name, img, price, description } = service;
@@ -7,12 +9,16 @@ const ServiceCard = ({ service }) => {
     <div>
       <div className="card w-full border border-solid outline-1">
         <div className="p-6">
-          <img
-            className="rounded-lg"
-            style={{ height: "208px", width: "318px" }}
-            src={img}
-            alt="Shoes"
-          />
+          <PhotoProvider>
+            <PhotoView src={img}>
+              <img
+                className="rounded-lg"
+                src={img}
+                style={{ objectFit: "cover" }}
+                alt=""
+              />
+            </PhotoView>
+          </PhotoProvider>
           <h2 className="card-title mt-5">{name}</h2>
           <div>
             {description.length > 100 ? (
