bd68eae981db3d06a108a6defe0dfb8ed7b81432
risaadodx <wrisaad@gmail.com>
2022-11-10 21:57:27 +0600

    myReviewsRow update

--- src/Pages/MyReviews/MyReviewsRow/MyReviewsRow.js
+++ src/Pages/MyReviews/MyReviewsRow/MyReviewsRow.js
@@ -3,19 +3,13 @@ import React from "react";
 const MyReviewsRow = ({ review }) => {
   const { serviceImg, serviceName, message } = review;
 
-  const handleDelete = (id) => {};
   return (
     <div>
       <tr>
         <td>
           <div className="flex items-center space-x-3">
             <div>
-              <button
-                onClick={() => handleDelete(_id)}
-                className="btn btn-ghost"
-              >
-                X
-              </button>
+              <button className="btn btn-ghost">X</button>
             </div>
             <div className="avatar">
               <div className="mask mask-squircle w-12 h-12">
